4bdd912e6007289bff32555168cf4c3d21828567
ashif88 <ashif62224@gmail.com>
2023-09-16 17:27:27 +0530

    Added policy_based_DS

---
+++ pbds.cpp
@@ -0,0 +1,20 @@
+/**
+ * author: Ashif Akhtar
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pb;
+#define fo find_by_order
+#define ok order_of_key
+#define ins insert
+
+// if you want multiset then make int to pair<int,int> in tree.
+
+signed main()
+{
+    return 0;
+}
